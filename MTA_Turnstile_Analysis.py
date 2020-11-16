037a3e241d5245cfa7c5495a8158e7db0096aed9
Priscilla Cheng <priscillacheng@Priscillas-MacBook-Pro.local>
2020-11-15 23:09:39 -0500

    Update content

--- MTA_Turnstile_Analysis.py
+++ MTA_Turnstile_Analysis.py
@@ -1,94 +1,68 @@
 import urllib
 import pandas as pd
-import numpy as np
 import re
 import requests
 from bs4 import BeautifulSoup
 
+
 url='http://web.mta.info/developers/turnstile.html'
 r=requests.get(url)
-soup = BeautifulSoup(r.text, 'html.parser')
+soup=BeautifulSoup(r.text, 'html.parser')
 print(soup.prettify())
 
-# get urls
+
+# Get urls
 for u in soup.find_all('a'):
     print (u.get('href'))
 
-# create list of urls containing txt data
+
+# Create list of urls containing txt data
 base_url='http://web.mta.info/developers/'
 links=[]
 
-for u in soup.find_all('a', attrs={'href': re.compile("^data")}):
+for u in soup.find_all('a', attrs={'href': re.compile("^data.*txt$")}):
     links.append(base_url+u.get('href'))
-print (*links[10], sep='\n')
+print (*links[:10], sep='\n')
 
-# specify data sets for year 2013
+
+# Specify data sets for year 2020
 year_2020=[]
 for link in range(len(links)):
-    if '_13' in links[link]:
+    if '_20' in links[link]:
         year_2020+=[links[link]]
-
-print(len(year_2020))
+print (len(year_2020))
 print (*year_2020, sep='\n')
 
 
-# preview
-df1 = pd.read_csv(year_2020[-1])
-df1
+# Preview one day data
+df_preview = pd.read_csv(year_2020[-1])
+print(df_preview.dtypes)
+df_preview.head(10)
 
-# creating data set
-total_records = []
+
+# Merge Multiple Data sets
+chars = ['\\r', '\\n', '\n', 'b\'', ' ','\'','"']
+total_records=[]
 for file in year_2020:
     with urllib.request.urlopen(file) as response:
         new_records = []
         for line in response:
-            line=str(line).replace(" ", "").replace("\\r\\n", "").replace("'","")
-            row = line.split(',')
-            first_three, remaining = row[:3], row[3:]
-            n = 5
-            new_record = [ first_three + remaining[i:i + n]
-                           for i in range(0, len(remaining), n)]
-            new_records += new_record
-        total_records += (new_records)
-df=pd.DataFrame(total_records, columns=["C/A","UNIT","SCP","DATE","TIME","DESC","ENTRIES","EXITS"])
+            for c in chars:
+                line = str(line).replace(c, "")
+            if line != 'C/A,UNIT,SCP,STATION,LINENAME,DIVISION,DATE,TIME,DESC,ENTRIES,EXITS':
+                new_records.append(line.split(','))
+        for record in new_records:
+            total_records.append(record)
 
-# Get rid of leading zeros for Exits Column
-df.head(100)
-df.shape #(203114, 8)
-df.sort_values(by='DATE')
-x=df.to_csv(r'./MTA_Turnstile.csv')
-type(df.DATE[0])
-print (df.DATE[0])
-print(year_2013)
+df = pd.DataFrame(total_records, columns=['C/A','UNIT','SCP','STATION','LINENAME','DIVISION','DATE','TIME','DESC','ENTRIES','EXITS'])
+df.shape # 9556799 rows x 11 columns
 
-# first_three = [a,b,c]
-# units_of_5 = [[1,2,3,4,5],[1,2,3,4,5]..]
-# new_record=[a,b,c,1,2,3,4,5]
-# total_records = [[a,b,c,1,2,3,4,5], ...]
+# Convert Columns' Data Types
+df['DATE']=pd.to_datetime(df['DATE'])
+df['EXITS']=df['EXITS'].astype('int64')
+df['ENTRIES']=df['ENTRIES'].astype('int64')
+df.dtypes
 
-# C/A = Control Area (A002)
-# UNIT = Remote Unit for a station (R051)
-# SCP = Subunit Channel Position represents an specific address for a device (02-00-00)
-# DATEn = Represents the date (MM-DD-YY)
-# TIMEn = Represents the time (hh:mm:ss) for a scheduled audit event
-# DEScn = Represent the "REGULAR" scheduled audit event (occurs every 4 hours)
-# ENTRIESn = The comulative entry register value for a device
-# EXISTn = The cumulative exit register value for a device
-
-#############################################################
-# Which station has the most number of units?
-# Answer: Wall Street
-df.loc[:,'UNIT'].mode()  #R043
-df.UNIT.value_counts().idxmax() #R043
-df.UNIT.value_counts().max()  #253221 occurrences
-
-# What is the total number of entries & exits across the subway system for February 1, 2013?
-df["DT"]=pd.to_datetime(df['DATE'] + ' ' + df['TIME'])
-df.sort_values(by=['DT'], inplace=True, ascending=True)
+# df.sort_values(by='DATE')
+# print(df['DATE'].unique())
 df.head(10)
-df.tail(10)
-df.index = df['DT']
-del df['DT']
-df['2013']
-df['2013'].ENTRIES.resample('M').sum()
-df['2013'].EXITS.resample('M').sum()
